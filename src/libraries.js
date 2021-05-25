b8a56ec24e04497a53a0558e606f23f3e280e4e9
andry_pebrianto <andrypeb227@gmail.com>
2021-05-25 07:35:11 +0700

    fix bug

--- src/libraries.js
+++ src/libraries.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, h) => {
   const response = h.response({
     status: 'error',
-    message: error.message,
+    message: `${error.name}: ${error.message}`,
   });
   response.code(500);
   return response;
